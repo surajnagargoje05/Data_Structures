46e237b241930ca7a2a6047582514fdc7a071a49
Suraj Nagargoje <91446342+surajnagargoje05@users.noreply.github.com>
2025-09-24 15:12:58 +0530

    Added node and linkedList classes

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+class Node{
+  public:
+    int value;
+    Node *next;
+    Node(int value){
+      this->value = value;
+      next = nullptr;
+    }
+};
+
+class LinkedList{
+  private:
+    Node *head;
+    Node *tail;
+    int length;
+  public:
+    LinkedList(int value){
+      Node *newNode = new Node(value);
+      head = newNode;
+      tail = newNode;
+      length = 0;
+};  
