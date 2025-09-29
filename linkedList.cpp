1b855e9f471ba7664539e870abfd511a0f1a89f0
Suraj Nagargoje <surajnagargoje05@gmail.com>
2025-09-29 15:12:57 +0530

    Updated code for append, prepend, deleteLast

--- linkedList.cpp
+++ linkedList.cpp
@@ -21,5 +21,87 @@ class LinkedList{
       Node *newNode = new Node(value);
       head = newNode;
       tail = newNode;
-      length = 0;
-};  
+      length = 1;
+    }
+
+    void AppendList(int value){
+      Node *newNode = new Node(value);
+      if(length == 0){
+        head = newNode;
+        tail = newNode;
+      }
+      else{
+        tail->next = newNode;
+        tail = newNode;
+      }
+      length++;
+    }
+
+    void PrintList(){
+      Node *temp = head;
+      while(temp){
+        cout << temp->value << " ";
+        temp = temp->next;
+      }
+      cout << endl;
+    }
+
+    void DeleteLast(){
+      if(length == 0){
+        return ;
+      }
+      if(length == 1){
+        head = nullptr;
+        tail = nullptr;
+      }
+      else{
+        Node *pre = head;
+        Node *temp = head;
+        while(temp->next){
+          pre = temp;
+          temp = temp->next;
+        }
+        tail = pre;
+        tail->next = nullptr;
+      }
+    }
+
+    void PrependList(int value){
+      Node *newNode = new Node(value);
+      if(length == 0){
+        head = newNode;
+        tail = newNode;
+        length++;
+      }
+      else{
+        newNode->next = head;
+        head = newNode;
+        length++;
+      }
+    }
+};
+
+int main(){
+  // Declared List
+  LinkedList list(100);
+
+  // Append List
+  list.AppendList(200);
+  list.AppendList(300);
+  list.AppendList(400);
+  list.AppendList(500);
+
+  //Print List
+  list.PrintList();
+
+  // Delete Last Node
+  list.DeleteLast();
+  list.PrintList();
+
+  // Prepend list
+  list.PrependList(140);
+  list.PrintList();
+
+
+  return 0;
+}
